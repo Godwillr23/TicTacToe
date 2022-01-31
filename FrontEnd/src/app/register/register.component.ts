0ca5be53ff2603d2b1b2ebd8738a21984a146066
Godwill <godwillr23@gmail.com>
2022-01-31 16:54:46 +0200

    Revert "Revert "Register and Login changes with form validations""
    This reverts commit 86583be8f005456756f9dedf1cdd3539cfc09c1f.

---
+++ FrontEnd/src/app/register/register.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';    
+import { LoginService } from '../login.service';    
+import {Register} from '../register';    
+import {Observable} from 'rxjs';    
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';    
+    
+@Component({    
+  selector: 'app-register',    
+  templateUrl: './register.component.html',    
+  styleUrls: ['./register.component.scss']    
+})    
+export class RegisterComponent implements OnInit {  
+
+  data = false;    
+  public UserForm: any;    
+  public message: any;    
+  errorMessage!: string;
+
+  constructor(
+    private formbulider: FormBuilder,
+    private loginService:LoginService) 
+  { }    
+    
+  ngOnInit() {    
+    this.UserForm = this.formbulider.group({    
+      UserName: ['', [Validators.required]],    
+      Password: ['', [Validators.required]]  
+    });    
+  }   
+
+   
+  onFormSubmit()    
+  {    
+    const user = this.UserForm.value;    
+    this.register(user);    
+  }  
+  
+  register(register:Register)    
+  {    
+    this.message = '';
+    this.errorMessage = '';
+
+      this.loginService.register(register).subscribe(
+      regdata => {        
+        if(regdata.Status == "Success")    
+        {       
+          this.data = true;    
+          this.message = 'Saved Successfully';    
+          this.UserForm.reset();        
+        }    
+        else{    
+          this.errorMessage = regdata.Message;    
+        }    
+      },    
+      error => {    
+        this.errorMessage = error.message;    
+      });    
+   
+  }    
+}    
